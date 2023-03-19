1756551a3665a1da0c058515d7a61c797e2609af
Marcos Kloss <marcoskloss2001@gmail.com>
2023-03-18 22:22:42 -0300

    alpha blending

---
+++ lazyfoo/13-alpha-blending/main.cpp
@@ -0,0 +1,232 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_blendmode.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_stdinc.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_video.h>
+#include <stdio.h>
+#include <string>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+// Check for a possible SDL error code
+void sdlc(int code)
+{
+    if (code < 0) {
+        fprintf(stderr, "ERROR (error code found): SDL did something wrong: %s\n", SDL_GetError());
+        exit(1);
+    }
+}
+
+// Check for a possible NULL pointer
+template <typename T>
+T* sdlcp(T* ptr) 
+{
+    if (ptr == NULL) {
+        fprintf(stderr, "ERROR (NULL pointer found): SLD did something wrong: %s\n", SDL_GetError());
+        exit(1);
+    }
+      return ptr;
+}
+
+void print_IMG_error_and_quit()
+{
+    printf("ERROR (SDL_image error): %s\n", IMG_GetError());
+    exit(1);
+}
+
+SDL_Window* g_window = NULL;
+SDL_Renderer* g_renderer = NULL;
+
+class Texture 
+{
+    public:
+        Texture();
+        ~Texture();
+
+        void load_from_file(std::string path);
+        void free();
+        void render(int x, int y, SDL_Rect* clip = NULL);
+        int get_width();
+        int get_height();
+        void set_color(Uint8 r, Uint8 g, Uint8 b);
+        void set_blend_mode(SDL_BlendMode blendmode);
+        void set_alpha(Uint8 a);
+
+    private:
+        SDL_Texture* texture;
+        int width;
+        int height;
+};
+
+Texture::Texture()
+{
+    texture = NULL;
+    width = 0;
+    height = 0;
+}
+
+Texture::~Texture()
+{
+    free();
+}
+
+void Texture::load_from_file(std::string path)
+{
+    free();
+    SDL_Surface* surface = IMG_Load(path.c_str());
+    if (surface == NULL) print_IMG_error_and_quit();
+
+    // Color key image (in that case cyan - 00FFFF)
+    SDL_SetColorKey(surface, SDL_TRUE, SDL_MapRGB(surface->format, 0, 0xFF, 0xFF));
+
+    texture = sdlcp(SDL_CreateTextureFromSurface(g_renderer, surface));
+    width = surface->w;
+    height = surface->h;
+    SDL_FreeSurface(surface);
+}
+
+void Texture::free()
+{
+    if (texture != NULL) {
+        SDL_DestroyTexture(texture);
+        texture = NULL;
+        width = 0;
+        height = 0;
+    }
+}
+
+void Texture::render(int x, int y, SDL_Rect* clip)
+{
+    SDL_Rect render_rect = { .x = x, .y = y, .w = width, .h = height };
+
+    // if we have a clip we need to set the size of the rendered texture to the size of the clip
+    if (clip != NULL) {
+        render_rect.w = clip->w;
+        render_rect.h = clip->h;
+    }
+
+    SDL_RenderCopy(g_renderer, texture, clip, &render_rect);
+}
+
+int Texture::get_width()
+{
+    return width;
+}
+
+int Texture::get_height()
+{
+    return height;
+}
+
+void Texture::set_color(Uint8 r, Uint8 g, Uint8 b)
+{
+    SDL_SetTextureColorMod(texture, r, g, b);
+}
+
+void Texture::set_blend_mode(SDL_BlendMode blendmode)
+{
+    SDL_SetTextureBlendMode(texture, blendmode);
+}
+
+void Texture::set_alpha(Uint8 alpha)
+{
+    SDL_SetTextureAlphaMod(texture, alpha);
+}
+
+void init()
+{
+    sdlc(SDL_Init(SDL_INIT_VIDEO));
+    g_window = sdlcp(SDL_CreateWindow(
+            "Alpha blending", 
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+            SCREEN_WIDTH, SCREEN_HEIGHT,
+            SDL_WINDOW_SHOWN));
+
+    g_renderer = sdlcp(SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED));
+    int flags = IMG_INIT_PNG;
+    int initted = IMG_Init(flags);
+    if ((initted & flags) != flags) print_IMG_error_and_quit();
+}
+
+void shutdown()
+{
+    SDL_DestroyRenderer(g_renderer);
+    SDL_DestroyWindow(g_window);
+    IMG_Quit();
+    SDL_Quit();
+}
+
+int main( int argc, char* args[] )
+{
+    init();
+
+    Texture modulated_texture;
+    Texture background_texture;
+
+    modulated_texture.load_from_file("./fadeout.png");
+    // set standard alpha blending
+    modulated_texture.set_blend_mode(SDL_BLENDMODE_BLEND);
+
+    // since the background is not going to be transparent
+    // we dont need to set a blend mode on it
+    background_texture.load_from_file("./fadein.png");
+
+    bool quit = false;
+    SDL_Event ev;
+
+    Uint8 alpha = 255;
+    Uint8 offset = 16;
+
+    while (quit == false) {
+        while (SDL_PollEvent(&ev)) {
+            if (ev.type == SDL_QUIT) quit = true;
+
+            if (ev.type == SDL_KEYDOWN) {
+                switch (ev.key.keysym.sym) {
+                // increase alpha on W
+                case SDLK_w:
+                {
+                    Uint8 next = alpha + offset;
+                    if (next > 255) {
+                        next = 255;
+                    }
+                    alpha = next;
+                    break;
+                }
+
+                // decrease alpha on S
+                case SDLK_s:
+                {
+                    Uint8 next = alpha - offset;
+                    if (next < 0) {
+                        next = 0;
+                    }
+                    alpha = next;
+                    break;
+                }
+                }
+            }
+        }
+
+        // clear screen
+        SDL_SetRenderDrawColor(g_renderer, 0xFF,0xFF,0xFF,0xFF);
+        SDL_RenderClear(g_renderer);
+
+        // the order is important
+        background_texture.render(0, 0);
+        modulated_texture.set_alpha(alpha);
+        modulated_texture.render(0, 0);
+        
+        SDL_RenderPresent(g_renderer);
+    }
+
+    shutdown();
+    return 0;
+}
+
